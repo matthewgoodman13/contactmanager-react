9e2bb9715ec9400cbc341980e34e5222f74fc289
Matthew Goodman <matthewgoodman13@outlook.com>
2019-04-24 15:30:24 -0400

    initial commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+
+  // https://medium.com/@baphemot/understanding-react-react-16-3-component-life-cycle-23129bc7a705
+
+  state = {
+    title: '',
+    body: ''
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => this.setState({
+    title: data.title,
+    body: data.body
+  }))
+  }
+
+  // componentWillMount() {
+  //   console.log("componentWillMount...");
+  // }
+
+  // componentDidUpdate() {
+  //   console.log("componentDidUpdate...");
+  // }
+
+  // componentWillUpdate() {
+  //   console.log("componentWillUpdate...");
+  // }
+
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('componentWillReceiveProps...')
+  // }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return null;
+  // }
+
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapshotBeforeUpdate...')
+  // }
+
+  render() {
+    const { title, body} = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    )
+  }
+}
+
+export default Test;
